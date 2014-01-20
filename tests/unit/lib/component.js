84e4c643e0678a0d79d12655945f4324e52dbaf9
Corey Frang <gnarf37@gmail.com>
2014-01-20 09:04:38 -0500

    Some skeleton for component inheritance

--- tests/unit/lib/component.js
+++ tests/unit/lib/component.js
@@ -1,4 +1,7 @@
 var component = require( "../../../lib/component" );
+var sinon = require("sinon");
+
+// var epoch = new Date(1980, 4, 27, 4);
 
 exports.factory = function( test ) {
 	test.expect( 2 );
@@ -6,3 +9,65 @@ exports.factory = function( test ) {
 	test.equal( typeof component.factory, "function", "factory is a method" );
 	test.done();
 };
+
+exports["test factory"] = {
+	setUp: function( done ) {
+		this.factory = component.factory({ guid: "test.factory" });
+		done();
+	},
+	tearDown: function( done ) {
+		delete this.factory;
+		done();
+	},
+	"test instance": function( test ) {
+		test.expect( 2 );
+		var instance = this.factory({ test: true });
+		test.equal( instance.test, true, "got instance variable" );
+		test.equal( instance.guid, "test.factory", "got factory variable" );
+		test.done();
+	},
+};
+
+function MockMetric( name ) {
+	return {
+		read: sinon.spy(),
+		clone: sinon.spy(function() {
+			return MockMetric( name );
+		})
+	};
+}
+
+exports["test metrics factory"] = {
+	setUp: function( done ) {
+		this.factory = component.factory({
+			guid: "test.metrics.factory",
+			metrics: {
+				test: MockMetric("test"),
+				testArray: [
+					MockMetric("testArray[0]"),
+					MockMetric("testArray[1]"),
+				]
+			}
+		});
+		done();
+	},
+	tearDown: function( done ) {
+		delete this.factory;
+		done();
+	},
+	"test instance": function( test ) {
+		test.expect( 8 );
+		var proto = this.factory.prototype;
+		test.ok( !proto.metrics.test.clone.called );
+		var instance = this.factory();
+		test.ok( proto.metrics.test.clone.called );
+		test.notEqual( instance.metrics, proto.metrics );
+		test.notEqual( instance.metrics.test, proto.metrics.test );
+		test.equal( instance.metrics.test.name, proto.metrics.test.name );
+		test.notEqual( instance.metrics.testArray, proto.metrics.testArray );
+		test.notEqual( instance.metrics.testArray[0], proto.metrics.testArray[0] );
+		test.equal( instance.metrics.testArray[0].name, proto.metrics.testArray[0].name );
+		test.done();
+	}
+};
+
